cf27a196e7ecd6f47e18ec1c007a7142335173dc
Josiah Cury <josiah.cury@student.nmt.edu>
2022-02-25 16:51:19 -0700

    Added grammar test cases.

--- hw3/testcases/WhileLoop.java
+++ hw3/testcases/WhileLoop.java
@@ -2,14 +2,8 @@ public class hello {
     int a;
     int i;
    public static void main(String argv[]) {
-      System.out.println("hello, jzero!");
-      a = 15;
       while(a){
           System.out.println("juicy\t");
       }
-      for(i=1; i < a; ){
-
-      }
-      i++;
    }
 }
