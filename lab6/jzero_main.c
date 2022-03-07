a5e3c773ace9887b786f2aa0e450b03a8f24dede
Josiah Cury <josiah.cury@student.nmt.edu>
2022-03-06 23:14:17 -0700

    Finished Lab 6. Made simple Symbol Table insertions.

---
+++ lab6/jzero_main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tree.h"
+#include "defs.h"
+#include "j0gram.tab.h"
+#include "error.h"
+#include "symt.h"
+
+extern int yydebug;
+
+int main(int argc, char *argv[]) {
+
+	if(argc == 1)
+		yyin = stdin;
+
+	while (--argc > 0) {
+
+		if ((yyin = fopen(*++argv, "r")) == NULL)
+    		return 0;
+
+    	yyfilename = *argv;
+    	yylineno = 1;
+
+    	//yydebug = 1;
+    	printf("Beginning parse of file: %s\n", yyfilename);
+    	yyparse();
+        print_tree(tree_root, 1);
+		//free_tree(tree_root);
+		printf("\n");
+
+		printf("Beginning print of all symbols in files!\n");
+		printsyms(tree_root);
+		printf("\n");
+
+		globals = new_st(10);
+		current = globals;
+		printf("Populating symbol tables!\n");
+		populate_symboltables(tree_root);
+		printf("\n");
+
+		printf("Printing symbol tables\n");
+		printsymbols(globals, 1);
+		printf("\n");
+
+		delete_st(globals);
+		printf("\n");
+    }
+    //printf("Category\t Text\t\t  Lineno   Filename\t\t Ival/Sval\n");
+	//printf("------------------------------------------------------------------\n");
+
+	fclose(yyin);
+	yylex_destroy();
+    return 0;
+}
+
+void check_extension(char *fname) {
+	int len = strlen(fname);
+	//printf("%d\n", len);
+
+	if(len <= 5 || strcmp(fname+len-5, ".java") != 0) {
+		strcat(fname, ".java");
+		//printf("%s\n", fname);
+	}
+}
---
+++ lab6/token.h
@@ -0,0 +1,28 @@
+#ifndef TOKEN_H_
+#define TOKEN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <errno.h>
+#include "tree.h"
+#include "j0gram.tab.h"
+
+struct token {
+	int category;   /* the integer code returned by yylex */
+	char *text;     /* the actual string (lexeme) matched */
+	int lineno;     /* the line number on which the token occurs */
+	char *filename; /* the source file in which the token occurs */
+	int ival;       /* for integer constants, store binary value here */
+	double dval;	   /* for real constants, store binary value here */
+	char *sval;     /* for string constants, malloc space, de-escape, store */
+		           /*    the string (less quotes and after escapes) here */
+};
+
+//token.c Function Declarations
+int error_handler(int lex_code);
+int init_token(int code);
+void text_eval(struct token *node);
+
+#endif
